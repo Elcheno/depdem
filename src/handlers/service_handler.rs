64da8dd1b754a8857245b115d74a16fe39f61599
Elcheno <r.c.a2312@gmail.com>
2024-11-01 18:27:34 +0100

    impl generic services in service_handler file

--- src/handlers/service_handler.rs
+++ src/handlers/service_handler.rs
@@ -2,58 +2,46 @@ use crate::models::jwt_model::JWT;
 use crate::models::response_model::{
     NetworkResponse, ResponseBody, ResponseError, ResponseSuccess,
 };
+use crate::models::service_dto_model::ServiceRequest;
+use crate::models::service_model::{ActionService, Service};
 use crate::utils::transform::transform_vec_to_string;
 use rocket::serde::json::{json, Json};
-use std::process::Command;
 
-#[get("/service/status")]
+#[get("/service/status", format = "json", data = "<service_request>")]
 pub fn service_status(
     key: Result<JWT, NetworkResponse>,
+    service_request: Json<ServiceRequest>,
 ) -> Result<Json<ResponseSuccess>, NetworkResponse> {
     let _jwt = key?;
-
-    let output = Command::new("sh")
-        .args(["-c", "service tomcat10 status"])
-        .output()
-        .expect("failed to execute process");
-
+    let service = Service::build(&service_request.into_inner(), ActionService::Status)?;
+    let output = service.execute();
     let code = output.status.code().unwrap();
-
     if code == 0 {
         return Ok(Json(ResponseSuccess::ok("Service is running".to_string())));
     }
-
-    let std_err: String = transform_vec_to_string(&output.stderr);
-
     Err(NetworkResponse::InternalServerError(
         json!(ResponseBody::Error(ResponseError::new(
             Some(code),
             String::from("Error checking service status"),
-            Some(std_err),
+            Some(transform_vec_to_string(&output.stderr)),
         )))
         .to_string(),
     ))
 }
 
-#[put("/service/stop")]
+#[put("/service/stop", format = "json", data = "<service_request>")]
 pub fn stop_service(
     key: Result<JWT, NetworkResponse>,
+    service_request: Json<ServiceRequest>,
 ) -> Result<Json<ResponseSuccess>, NetworkResponse> {
     let _jwt = key?;
-
-    let output = Command::new("sh")
-        .args(["-c", "service tomcat10 stop"])
-        .output()
-        .expect("failed to execute process");
-
+    let service = Service::build(&service_request.into_inner(), ActionService::Stop)?;
+    let output = service.execute();
     let code = output.status.code().unwrap();
-
     if code == 0 {
         return Ok(Json(ResponseSuccess::ok("Service stopped".to_string())));
     }
-
     let std_err: String = transform_vec_to_string(&output.stderr);
-
     Err(NetworkResponse::InternalServerError(
         json!(ResponseBody::Error(ResponseError::new(
             Some(code),
@@ -64,25 +52,19 @@ pub fn stop_service(
     ))
 }
 
-#[put("/service/start")]
+#[put("/service/start", format = "json", data = "<service_request>")]
 pub fn start_service(
     key: Result<JWT, NetworkResponse>,
+    service_request: Json<ServiceRequest>,
 ) -> Result<Json<ResponseSuccess>, NetworkResponse> {
     let _jwt = key?;
-
-    let output = Command::new("sh")
-        .args(["-c", "service tomcat10 start"])
-        .output()
-        .expect("failed to execute process");
-
+    let service = Service::build(&service_request.into_inner(), ActionService::Start)?;
+    let output = service.execute();
     let code = output.status.code().unwrap();
-
     if code == 0 {
         return Ok(Json(ResponseSuccess::ok("Service started".to_string())));
     }
-
     let std_err: String = transform_vec_to_string(&output.stderr);
-
     Err(NetworkResponse::InternalServerError(
         json!(ResponseBody::Error(ResponseError::new(
             Some(code),
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,5 @@
 pub mod jwt_model;
 pub mod path_model;
 pub mod response_model;
+pub mod service_dto_model;
+pub mod service_model;
